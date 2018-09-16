17bb64c806596f586f6712f8faf1f29d155e7399
adomant <m.facepalm@gmail.com>
2018-09-16 12:13:29 +0300

    indent fixes

--- lesson2/task_2.rb
+++ lesson2/task_2.rb
@@ -5,16 +5,18 @@ require 'rspec/core'
 
 
 class FileCreater
-
   def initialize(hash)
     @hash = hash
   end
+
   def headers_creation
     @headers = @hash[:headers]
   end
+
   def body_creation
     @body = @hash[:body]
   end
+
   def csv_creation
     @s = CSV.generate do |csv|
       csv << @headers
@@ -23,12 +25,15 @@ class FileCreater
     end
     end
   end
+
   def csv_save
     File.write('the_file.csv', @s)
   end
+
   def xls_save
     File.write('the_file.xls', @s)
   end
+
 end
 
 h = {
@@ -46,7 +51,7 @@ instance.xls_save
 
 
 RSpec.describe FileCreater do
-  
+
   let(:h) { {
     headers: ['First Name', 'Last Name', 'Age'],
     body: { '0' => ['Dima', 'Strukov', 22], '1' => ['Mikhail', 'Matyukhin', 22]
@@ -56,12 +61,12 @@ RSpec.describe FileCreater do
   let(:instance1) { described_class.new(h) }
 
   describe 'headers_creation' do
-    subject { instance.headers_creation}
+    subject { instance1.headers_creation}
     it { expect(subject).to eq ["First Name", "Last Name", "Age"] }
   end
 
   describe 'body_creation' do
-    subject { instance.body_creation}
+    subject { instance1.body_creation}
     it { expect(subject).to eq "0" => ["Dima", "Strukov", 22], "1" => ["Mikhail", "Matyukhin", 22] }
   end
 end
